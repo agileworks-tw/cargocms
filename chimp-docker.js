e5423445cf80bf3eaf7a999171df482f5574f49a
smlsunxie <smlsun@gmail.com>
2016-09-19 23:01:32 +0800

    update

--- chimp-docker.js
+++ chimp-docker.js
@@ -3,12 +3,12 @@
 // var host = ip.address();
 
 module.exports = {
-  // port: 4444,
-  // host: "e2e-env",
+  port: 4444,
+  host: "e2e-env",
   browser: 'firefox',
   screenshotPath: '/app/test/e2e/report/',
   webdriverio: {
-    // baseUrl: 'http://debug:1338',
+    baseUrl: 'http://debug:1338',
     waitforTimeout: 5000,
     screenshotPath: __dirname+"/test/e2e/screenshot"
   },
